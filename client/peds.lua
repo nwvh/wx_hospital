84d12479a4b2e35139b2ceef80716bef3b664530
wx <76164598+nwvh@users.noreply.github.com>
2023-09-14 21:21:28 +0200

    Update peds.lua

--- client/peds.lua
+++ client/peds.lua
@@ -2,7 +2,7 @@ Citizen.CreateThread(function ()
     for k,v in pairs(wx.Peds) do
         RequestModel(v.Model)
         while not HasModelLoaded(v.model) do Citizen.Wait(10) end
-        doctor = CreatePed(0,v.Model,v.Coords.x-0.21,v.Coords.y,v.Coords.z-1,v.Heading,true,true)
+        doctor = CreatePed(0,v.Model,v.Coords.x-0.21,v.Coords.y,v.Coords.z-1,v.Heading,false,true)
         FreezeEntityPosition(doctor, true)
         SetEntityInvincible(doctor,true)
         SetBlockingOfNonTemporaryEvents(doctor,true)
